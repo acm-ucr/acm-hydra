246b600ec3bda908487654bbfeef8f17f828d00e
rushilshah11 <rushilshah.282@gmail.com>
2022-04-29 18:04:35 -0700

    updated WhatWeDo implementation

--- src/components/About/WhatWeDo.js
+++ src/components/About/WhatWeDo.js
@@ -4,9 +4,9 @@ import {Row, Col} from "react-bootstrap"
 
 const WhatWeDo = () => {
     return (
-        <div>
+        <div className="about-WhatWeDo">
             <div>
-                <div className="WhatWeDo-header">
+                <div className="about-WhatWeDo-header">
 
                     <h1>What We Do</h1>
 
@@ -17,37 +17,37 @@ const WhatWeDo = () => {
                         Every quarter, we focus on improving specific aspects of our member.
                     </h3>
                 </div>
-        <Row>
 
-                {/* <div className="category-grid"> */}
-                    <Col lg = {4} sm={12}>
-                        <h2>Professional Development</h2>
-                        <ul>
-                            <li>Develop Professional Resumes</li>
-                            <li>Build all-star LinkedIns</li>
-                            <li>Offer workshops on applying effectively at companies</li>
-                        </ul>
-                    </Col>
+                <div className="about-WhatWeDo-grid">
+                    <Row>
+                        <Col lg = {4} sm={12} className="about-WhatWeDo-single-col">
+                            <h2>Professional Development</h2>
+                            <ul>
+                                <li>Develop Professional Resumes</li>
+                                <li>Build all-star LinkedIns</li>
+                                <li>Offer workshops on applying effectively at companies</li>
+                            </ul>
+                        </Col>
 
-                    <Col lg = {4} sm={12}>
-                        <h2>Workshops</h2>
-                        <ul >
-                            <li>Web/App Development</li>
-                            <li>Intro to Front-End/Back-End</li>
-                            <li>LeetCode 101</li>
-                        </ul>
-                    </Col>
+                        <Col lg = {4} sm={12} className="about-WhatWeDo-single-col">
+                            <h2>Workshops</h2>
+                            <ul >
+                                <li>Web/App Development</li>
+                                <li>Intro to Front-End/Back-End</li>
+                                <li>LeetCode 101</li>
+                            </ul>
+                        </Col>
 
-                    <Col  lg = {4} sm={12}>
-                        <h2>And More...</h2>
-                        <ul>
-                            <li>Offline Editing</li>
-                            <li>Source Control</li>
-                            <li>Utilizing API's</li>
-                        </ul>
-                    </Col>
-                {/* </div> */}
-            </Row>
+                        <Col  lg = {4} sm={12} className="about-WhatWeDo-single-col">
+                            <h2>And More...</h2>
+                            <ul>
+                                <li>Offline Editing</li>
+                                <li>Source Control</li>
+                                <li>Utilizing API's</li>
+                            </ul>
+                        </Col>
+                </Row>
+                </div>
             </div>
         </div>
         
