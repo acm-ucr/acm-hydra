2ac94e11f201f23c647f2c750bbed101cdd08032
Tech Visionaries <mrsiddhantgupta@gmail.com>
2022-04-29 18:13:50 -0700

    Started the forms part, worked on the title

---
+++ src/components/Events/SuggestionsForm.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const SuggestionsForm = () => {
+    return (
+        <div className="eventsForm">
+            <form>
+                <fieldset>
+                    <label>
+                        <h3>Name</h3>
+                        <input name="name " />
+                    </label>
+                </fieldset>
+            </form>
+        </div>
+    )
+}
+export default SuggestionsForm
