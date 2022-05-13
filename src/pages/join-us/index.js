3fa9288e69e4ee8838fd6e7d05fb4977c498cb6c
Zach Green <zachgreen@ucr-secure-43-10-13-220-78.wnet.ucr.edu>
2022-05-13 16:34:20 -0700

    implement cs orgs links

--- src/pages/join-us/index.js
+++ src/pages/join-us/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Row, Col} from 'react-bootstrap'
+import Links from "../../components/Join-Us/Links"
+// import "Style.css"
 
 const Join_Us = () => {
   return (
-    <div>JOIN US</div>
+    <div className="join-us title wrapper">
+    <Links />
+    </div>
+    
   )
 }
 
