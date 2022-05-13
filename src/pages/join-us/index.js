de1dd1eb538e0318dcf90d8e239828d34b32da09
lxyna <leynadiep@gmail.com>
2022-05-13 16:41:17 -0700

    start on clubroom info

--- src/pages/join-us/index.js
+++ src/pages/join-us/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
+import {Row, Col} from 'react-bootstrap'
+import ClubroomInfo from "../../components/Join-Us/ClubroomInfo"
+// import "Style.css"
 
 const Join_Us = () => {
   return (
-    <div>JOIN US</div>
+    <div className="join-us title wrapper">
+    <ClubroomInfo />
+    </div>
+
   )
 }
 
