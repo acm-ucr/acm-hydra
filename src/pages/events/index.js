8096fba5b8a7247169a6d07cbf2b0c3e9a2ade34
Prathmesh Jain <prathmeshjain02@gmail.com>
2022-05-13 17:36:27 -0700

    Events Card

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,24 +1,18 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Footer from '../../components/Footer/Footer';
 import Navigation from '../../components/Navigation/Navigation';
 
 import CalendarPageOne from '../../components/Events/CalendarPageOne';
+import CalendarPage2 from '../../components/Events/CalendarPage2';
 
 import SuggestionsForm from '../../components/Events/SuggestionsForm';
 
 const Events = () => {
-    /*
-    const [number, setNumber] = useState(0);
-
-    const increaseNumber = () => {
-        setNumber(number + 1)
-    }
-    */
-
     return (
         <div>
             <Navigation />
             <CalendarPageOne />
+            <CalendarPage2 />
             <SuggestionsForm />
             <Footer />
         </div>
