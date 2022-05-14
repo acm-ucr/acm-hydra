397d21ce3816710e81a07c89d3cc76ea1b4a2bc5
Zach Green <zachgreen@ucr-secure-43-10-13-220-78.wnet.ucr.edu>
2022-05-13 18:06:00 -0700

    implement cs orgs links

--- src/components/Join-Us/Links.js
+++ src/components/Join-Us/Links.js
@@ -7,24 +7,25 @@ const Links = () => {
   return (
     <div>
 
-    <div className="joinus-links-header" >
-    <p><big> You should join ACM @ UCR</big></p>
+      <div className="joinus-links-header" >
+        <p>Join ACM@UCR</p>
+      </div>
+      <div className="joinus-links-image">
+         <img src="https://i.imgur.com/1aKMX30.png" width="15%" height="15%" alt="ACM @ UCR Logo"></img> 
+      </div>
+      <br /><br />
+      <div className="joinus-links-orgs">
+        <p>Affiliated Organizations</p>
+        <a className="joinus-links-cs-orgs" href="https://cyber.cs.ucr.edu/" target="_parent">Cyber @ UCR</a>
+        <a className="joinus-links-cs-orgs" href="https://winc.cs.ucr.edu/" target="_parent">WINC</a>
+        <a className="joinus-links-cs-orgs" href="https://gamespawn.github.io/" target="_parent">Gamespawn</a>
+        <a className="joinus-links-cs-orgs" href="https://ieee.ucr.edu/" target="_parent">IEEE</a>
+        <a className="joinus-links-cs-orgs" href="https://ucrsolarcar.com/" target="_parent">Solar Car</a>
+        <a className="joinus-links-cs-orgs" href="https://ucrswe.org/" target="_parent">SWE</a>
+      </div>
     </div>
-    <div className="joinus-links-image">
-    <center><img src="https://i.imgur.com/1aKMX30.png" width="20%" height="20%" alt="ACM @ UCR Logo"></img></center>
-    </div>
-    <br></br>
-    <br></br>
-    <center><a href="https://cyber.cs.ucr.edu/" target="_parent"><button>Cyber @ UCR</button></a></center>
-    <center><a href="https://winc.cs.ucr.edu/" target="_parent"><button>WINC</button></a></center>
-    <center><a href="https://gamespawn.github.io/" target="_parent"><button>Gamespawn</button></a></center>
-    <center><a href="https://ieee.ucr.edu/" target="_parent"><button>IEEE</button></a></center>
-    <center><a href="https://ucrsolarcar.com/" target="_parent"><button>Solar Car</button></a></center>
-    <center><a href="https://ucrswe.org/" target="_parent"><button>SWE</button></a></center>
-    
 
-    </div>  
-    )
+  )
 }
 
 export default Links
