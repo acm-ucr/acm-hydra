e667e77653b872802a3215573a7fd044093ecac0
Tech Visionaries <mrsiddhantgupta@gmail.com>
2022-05-25 19:19:29 -0700

    Worked on creating input elements and created a row format for the suggestion form, but need to work on having the suggestion form be presented in different screen sizes..

--- src/components/Events/SuggestionsForm.js
+++ src/components/Events/SuggestionsForm.js
@@ -6,17 +6,17 @@ const SuggestionsForm = () => {
         <div className="events-suggestionsform-wrapper">
             <form>
                 <fieldset>
-                    <label>
+                    <label id="events-suggestionsform-gridwrapper">
                         <div className="events-suggestionsform-name">
-                            <h4>Name</h4>
+                            <label for="name">Name</label>
                             <input type="text" id="name" name="name"/>
                         </div>
                         <div className="events-suggestionsform-email">
-                            <h4>Email</h4>
+                            <label for="email">Email</label>
                             <input type="email" id="email" email="email"/>
                         </div>
                         <div className="events-suggestionsform-suggestion">
-                            <h4>Suggestion</h4>
+                            <label for="suggestion">Suggestion</label>
                             <textarea id="suggestion" suggestion="suggestion"/>
                         </div>
                         <div className="events-suggestionsform-submit">
