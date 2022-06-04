4b34061fe4782e562d12c517dadd919cab3b95d8
Tech Visionaries <mrsiddhantgupta@gmail.com>
2022-06-03 18:08:06 -0700

    Worked on storing user responses, and still in the process of working on the submission confirmation message.

--- src/components/Events/SuggestionsForm.js
+++ src/components/Events/SuggestionsForm.js
@@ -1,44 +1,64 @@
-import React from "react";
+import React, { useImperativeHandle, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import "./SuggestionsForm.css"
 
 const SuggestionsForm = () => {
-    return (
-        <div className="events-suggestionsform-wrapper">
-            <form>
-                <fieldset>
-                    <label id="events-suggestionsform-gridwrapper">
-                    <Container className="events-suggestionsform-boostrapwrapper" fluid>
-                        <Row>
-                            <Col>
-                                <div className="events-suggestionsform-name">
-                                    <label for="name">Name</label>
-                                    <input type="text" id="name" name="name"/>
-                                </div>
-                            </Col>
-                            <Col>
-                                <div className="events-suggestionsform-email">
-                                    <label for="email">Email</label>
-                                    <input type="email" id="email" email="email"/>
-                                </div>
-                            </Col>
-                            <Col>
-                                <div className="events-suggestionsform-suggestion">
-                                    <label for="suggestion">Suggestion</label>
-                                    <textarea id="suggestion" suggestion="suggestion"/>
-                                </div>
-                            </Col>
-                            <Col>
-                                <div className="events-suggestionsform-submit">
-                                    <button type="button">Submit</button>
-                                </div>
-                            </Col>
-                        </Row>
-                    </Container>
-                    </label>
-                </fieldset>
-            </form>
 
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [suggestion, setSuggestion] = useState("");
+    const [submit, setSubmit] = useState(false);
+
+    const handleSubmit = () => {
+        console.log("Name: " + name)
+        console.log("Email: " + email)
+        console.log("Suggestion: " + suggestion)
+        setSubmit(true);
+        /*name != null && email != null && suggestion != null ? <h3> Thank you for your suggestion! An officer will get back to you shortly!" </h3> : <h3> Please complete all fields before submitting! </h3>; */
+    }
+
+    // const VerifyInformation = () => {
+    //     ({ name } !== null || { email } !== null || { suggestion } !== null)
+    // }
+    return (
+
+
+        <div className="events-suggestionsform-wrapper">
+
+            <Container className="events-suggestionsform-boostrapwrapper" fluid>
+                <Row>
+                    <Col>
+                        <div className="events-suggestionsform-name">
+                            <label for="name">Name</label>
+                            <input type="text" id="name" name="name" value={name} placeholder="name" onChange={e => setName(e.target.value)} />
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="events-suggestionsform-email">
+                            <label for="email">Email</label>
+                            <input type="email" id="email" email="email" value={email} placeholder="email" onChange={e => setEmail(e.target.value)} />
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="events-suggestionsform-suggestion">
+                            <label for="suggestion">Suggestion</label>
+                            <textarea id="suggestion" suggestion="suggestion" value={suggestion} placeholder="suggestion" onChange={e => setSuggestion(e.target.value)} />
+                        </div>
+                    </Col>
+                    <Col>
+                        <div className="events-suggestionsform-submit">
+                            <button onClick={handleSubmit} type="button" id="submit">Submit</button>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+            {
+                submit ? 
+                    <h3> Thank you for your suggestion! An officer will get back to you shortly! </h3>
+                 : <></>
+                    // <h3> Please complete all fields before submitting! </h3>
+                
+            }
         </div>
     )
 }
