db10c922b19192c776fcccc067a9fb866ef63049
Divyank Shah <divyank.shah.2016@gmail.com>
2022-06-18 02:24:16 -0700

    fix up links

--- src/components/Join-Us/Links.js
+++ src/components/Join-Us/Links.js
@@ -6,61 +6,27 @@ import Link from './Link'
 const Links = () => {
 
   const links = [
-    ["https://cyber.cs.ucr.edu/", "Cyber @ UCR"],
-    ["https://winc.cs.ucr.edu/", "WINC"],
-    ["https://gamespawn.github.io/", "Gamespawn"],
-    ["https://ieee.ucr.edu/", "IEEE"],
-    ["https://ucrsolarcar.com/", "Solar Car"],
-    ["https://ucrswe.org/", "SWE"],
+    ["https://cyber.cs.ucr.edu/", "Cyber @ UCR", "Cyber"],
+    ["https://winc.cs.ucr.edu/", "WINC", "Women in Computing"],
+    ["https://gamespawn.github.io/", "Gamespawn", "Gamespawn"],
+    ["https://ieee.ucr.edu/", "IEEE", "Institute of Electrical Engineers"],
+    ["https://ucrswe.org/", "SWE", "Society of Women Engineers"],
   ]
 
   return (
     <div>
-      {/* <div className="joinus-links-header" >
-        <p>Join ACM@UCR</p>
-      </div>
-      <div className="joinus-links-image">
-         <img src="https://i.imgur.com/1aKMX30.png" width="15%" height="15%" alt="ACM @ UCR Logo"></img> 
-      </div> */}
-      <br /><br />
       <div className="joinus-links-orgs">
         <p>Affiliated Organizations</p>
         <Container fluid style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
           <Row style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
               {links.map(link => (
-                  <Col style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                      <Link link={link[0]} name={link[1]}  />
-                      <a>className="joinus-links-cs-orgs"</a>
+                  <Col xl = {4} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                      <Link link={link[0]} abbreviation={link[1]} name={link[2]} />
                   </Col>
               )
               )}
           </Row>
-        </Container>
-        
-        {/* lg={3} md={4} sm={6} xs={12} */}
-
-        {/* <Row>
-          <Col>
-          <a className="joinus-links-cs-orgs" href="https://cyber.cs.ucr.edu/" target="_parent">Cyber @ UCR</a>
-          </Col>
-          <Col>
-          <a className="joinus-links-cs-orgs" href="https://winc.cs.ucr.edu/" target="_parent">WINC</a>
-          </Col>
-          <Col>
-          <a className="joinus-links-cs-orgs" href="https://gamespawn.github.io/" target="_parent">Gamespawn</a>
-          </Col>
-          <Col>
-          <a className="joinus-links-cs-orgs" href="https://ieee.ucr.edu/" target="_parent">IEEE</a>
-          </Col>
-          <Col>
-          <a className="joinus-links-cs-orgs" href="https://ucrsolarcar.com/" target="_parent">Solar Car</a>
-          </Col>
-          <Col>
-          <a className="joinus-links-cs-orgs" href="https://ucrswe.org/" target="_parent">SWE</a>
-          </Col>
-
-          </Row> */}
-          
+        </Container>    
       </div>
     </div>
 
