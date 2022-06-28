43eeddb6659e6917e8d03617f35c7dc81c6a159c
Divyank Shah <divyank.shah.2016@gmail.com>
2022-06-28 02:00:00 -0700

    fix github actions

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,28 @@
 module.exports = {
-    env: {
-        browser: true,
-        es2021: true,
-    },
-    extends: ["plugin:react/recommended", "google", "prettier",
-        "next/core-web-vitals"],
-    parserOptions: {
-        ecmaFeatures: {
-            jsx: true,
-        },
-        ecmaVersion: 12,
-        sourceType: "module",
-    },
-    plugins: ["react"],
-    rules: {
-        "require-jsdoc": "off",
-    },
-    settings: {
-        react: {
-            version: "detect",
-        },
-    },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		"plugin:react/recommended",
+		"google",
+		"prettier",
+		"next/core-web-vitals",
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: "module",
+	},
+	plugins: ["react"],
+	rules: {
+		"require-jsdoc": "off",
+	},
+	settings: {
+		react: {
+			version: "detect",
+		},
+	},
 };
