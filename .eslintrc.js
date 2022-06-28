2fe295c20caca335c307bc3a54f2d0ad59f06de3
Divyank Shah <divyank.shah.2016@gmail.com>
2022-06-28 01:58:18 -0700

    fix linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,23 +1,24 @@
 module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-	},
-	extends: ["plugin:react/recommended", "google", "prettier"],
-	parserOptions: {
-		ecmaFeatures: {
-			jsx: true,
-		},
-		ecmaVersion: "latest",
-		sourceType: "module",
-	},
-	plugins: ["react"],
-	rules: {
-		"require-jsdoc": "off",
-	},
-	settings: {
-		react: {
-			version: "detect",
-		},
-	},
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["plugin:react/recommended", "google", "prettier",
+        "next/core-web-vitals"],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: "module",
+    },
+    plugins: ["react"],
+    rules: {
+        "require-jsdoc": "off",
+    },
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
 };
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import * as React from "react";
+import React from "react";
 import Home from "../components/Home/Home";
 // import "bootstrap/dist/css/bootstrap.min.css";
 
 const IndexPage = () => {
-	return (
-		<div className=''>
-			<Home />
-		</div>
-	);
+    return (
+        <div className=''>
+            <Home />
+        </div>
+    );
 };
 
 export default IndexPage;
