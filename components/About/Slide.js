840b2bff873fa35189ea0c726d807b34126d8355
Divyank Shah <divyank.shah.2016@gmail.com>
2022-07-04 17:56:00 -0700

    refactor faq using mambaui

--- components/About/Slide.js
+++ components/About/Slide.js
@@ -4,11 +4,11 @@ import styles from "./Slide.module.css";
 // make carousel
 
 const Slide = props => {
-	return (
-		<div className={styles.aboutSingleSlide}>
-			{/* <img src={props.image} alt="prop" /> */}
-		</div>
-	);
+    return (
+        <div className={styles.aboutSingleSlide}>
+            {/* <img src={props.image} alt="prop" /> */}
+        </div>
+    );
 };
 
 export default Slide;
