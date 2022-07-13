d0f4aee2e0fb4a0d0027a5b65c483a5280b679a0
Divyank Shah <divyank.shah.2016@gmail.com>
2022-07-12 17:32:12 -0700

    formatting

--- components/Forms/DUMMY.js
+++ components/Forms/DUMMY.js
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from "react";
 
 const DUMMY = () => {
-    return (
-        <div>DUMMY</div>
-    )
-}
+	return <div>DUMMY</div>;
+};
 
-export default DUMMY
+export default DUMMY;
