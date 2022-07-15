108f9694de3378291d32a21f6f33459d64161f9b
Nicholas Tengbumroong <cherryappletruck@gmail.com>
2022-07-15 14:38:44 -0700

    Removed comments

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -14,22 +14,7 @@ const Footer = () => {
 									src='https://acmucr.org/src/images/acm_ucr_logo.png'
 									alt='ACM Logo'
 								/>
-								{/*
-								<svg
-									xmlns='http://www.w3.org/2000/svg'
-									viewBox='0 0 32 32'
-									fill='currentColor'
-									className='flex-shrink-0 w-5 h-5 rounded-full text-gray-50'
-								>
-									<path d='M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z'></path>
-								</svg>
-                */}
 							</div>
-							{/*
-							<span className='self-center text-2xl font-semibold'>
-								Brand name
-							</span>
-							*/}
 						</a>
 					</div>
 					<div className='col-span-6 text-center md:text-left md:col-span-3'>
