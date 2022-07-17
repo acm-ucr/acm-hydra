2bfe0df5f489406620c11ba0bfaa03c0ec796d72
Sarah Ramirez <sarrami270@gmail.com>
2022-07-17 12:45:07 -0700

    remove privacy policy and terms of serv.

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -113,12 +113,6 @@ const Footer = () => {
 				<div className='grid justify-center pt-6 lg:justify-between'>
 					<div className='flex flex-col self-center text-sm text-center md:block lg:col-start-1 md:space-x-6'>
 						<span>©2022 All rights reserved</span>
-						<a rel='noopener noreferrer' href='#'>
-							<span>Privacy policy</span>
-						</a>
-						<a rel='noopener noreferrer' href='#'>
-							<span>Terms of service</span>
-						</a>
 					</div>
 					<div className='flex justify-center pt-4 space-x-4 lg:pt-0 lg:col-end-13'>
 						<a
