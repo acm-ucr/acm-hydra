058f5fd11f828a954531d4329e61405aee66d5fa
Val <vwong031@ucr.edu>
2022-07-18 17:19:03 -0700

    Populated dummy images. Finish adding alt/clean up code

--- components/About/Gallery.js
+++ components/About/Gallery.js
@@ -5,52 +5,52 @@ const Gallery = () => {
 		<section className='py-6 dark:bg-gray-800 dark:text-gray-50'>
 			<div className='container grid grid-cols-2 gap-4 p-4 mx-auto md:grid-cols-4'>
 				<img
-					src='https://source.unsplash.com/random/301x301/'
+					src='https://lh3.googleusercontent.com/pw/AM-JKLVv7AS5gdRC3kZ2lN-_QtsiZVKcnfteGQkqnZqANnOmhAxdUEjImIgs_lFf7-L3PnlHPLnUxdkMk80y2kEFCxifGZ4eGddL9gpupz5PFMeTqHuwQ-AYH-tz0TpoP9WErROIPx2Jjo1Sbjx1yfeozyMt4w=w1244-h933-no?authuser=0'
 					alt=''
 					className='w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-3 md:row-start-1 dark:bg-gray-500 aspect-square'
 				/>
 				<img
 					alt=''
 					className='w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square'
-					src='https://source.unsplash.com/random/200x200/?0'
+					src='https://lh3.googleusercontent.com/pw/AM-JKLVJyiU05MtXcvINZhVmjKw6G5-ems6zapmWuVbRguDIjcsl0v1IFWBYwVBTVuSt0LIeCLgxWcxevJ1UErlaJrJQ2cs6qrKTG2zwG3Vo9r6mIS7Sj5IenkSbMhqHhmq903mjlclLB0ZbF6GS6HgH4XcYgg=w1244-h933-no?authuser=0'
+				/>
+				<img
+					alt='A group of students in a classroom watching two people give a presentation.'
+					className='w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square'
+          src='https://lh3.googleusercontent.com/pw/AM-JKLWKcRa4bq-Hh5bC53PoFSaB7l9Go00v6BmY4D8DjDq-Hd3-y3lI89lEAWOBLvF5ea7CB-A9c9tk0zLmwWLmfZwLI2PCVU7L81S8SX3TvvQIDLFc9eR_BPSYjJ3XKDTE_wZqgBhvfCDyejsTbJB_3-2_Xg=w1244-h933-no?authuser=0'
+				/>
+				<img
+					alt='Professors oversee as students group up and gather around tables to brainstorm ideas.'
+					className='w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square'
+					src='https://lh3.googleusercontent.com/pw/AM-JKLUxTDqte6gXA_0W6MYooEHyUToc8hJWHDF8o68OgP9C3u-SMcHeT0S1HL5EFscAE_aCbAm1BT9KJNmvaqleWbmSZ_ZVvb4eTfC_YWdp2uMhq4-pzZNJo94g6BOT0Nzqorlg4FIepSQYjjS_upkXj5cIrA=w1244-h933-no?authuser=0'
 				/>
 				<img
 					alt=''
 					className='w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square'
-					src='https://source.unsplash.com/random/200x200/?1'
+					src='https://lh3.googleusercontent.com/pw/AM-JKLW-c7w_41ZXhM42gkUCBdTzyGeR4jVljo_Sups1N3RO8jSg-1JAaNUUrCG_Vb8Kk2R5n_rg-qskJla5qIiDoXXt90p7YeiCi0STmSJ3kuNvtFvT7DSNJRUNekUzSZXqJOqXcf0VcIk4IHh4WM6lq8uqXQ=w1244-h933-no?authuser=0'
 				/>
 				<img
 					alt=''
 					className='w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square'
-					src='https://source.unsplash.com/random/200x200/?2'
+					src='https://lh3.googleusercontent.com/pw/AM-JKLUssDBaIJebnVcndj0MvMhSm3-YLjju_khHNF23RMljvbbqWSDR1qRata0Cars3k2FNUUihwrBIG-sRYpWLVWQQOCe8qt1tae3aLud3RWLgpqHq09V6bTUDW-DlH9fc3yOIp9sixSZDJ7nuW0yj9tsizw=w1244-h933-no?authuser=0'
 				/>
 				<img
 					alt=''
 					className='w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square'
-					src='https://source.unsplash.com/random/200x200/?3'
+					src='https://lh3.googleusercontent.com/pw/AM-JKLUxFh3nc78DJ2V85bG10S2roi-Wk4B-jxPMj8EAA7xbjObtGFBlJOYULpZulnpwWNIPpfkjxlrq9jTMYdt5jw3PVWne58w8NmlS86h1JQUvaTHXRusqodMc4kVTt9AHqjhBPoCI0s3-tQ2ffejeiw2rVw=w1244-h933-no?authuser=0'
 				/>
 				<img
 					alt=''
 					className='w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square'
-					src='https://source.unsplash.com/random/200x200/?4'
+          src='https://lh3.googleusercontent.com/pw/AM-JKLWVf9Xx0iPnS59G-6gqjh1Z-y7TSr5B3Ttq70RC8JGithzOr2ibK5WykjbZTic4BQGBAnEy7-dGfIYXEoGAPAbHxuRs4mSMB6ghv7DDitlaMBewVbpT2ND6N4IpPtm5har4r5AvXgighdnyt_nwiwD4WA=w1244-h933-no?authuser=0'
 				/>
 				<img
 					alt=''
 					className='w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square'
-					src='https://source.unsplash.com/random/200x200/?5'
+					src='https://lh3.googleusercontent.com/pw/AM-JKLX05apGbDHyBlp88nG3_yMGc4NJfHq6HRhqUasWLJp_xzxUBpijeKr2T-ayoNNHJqH7rGEXkYv0iNLQUYPOtakVpogD5lw0uvZ7sE1NkoONhqKSOVM58jvoy82oEE0n-dP9lSa_gtHRajiGU5qn3bCIng=w1244-h933-no?authuser=0'
 				/>
 				<img
-					alt=''
-					className='w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square'
-					src='https://source.unsplash.com/random/200x200/?6'
-				/>
-				<img
-					alt=''
-					className='w-full h-full rounded shadow-sm min-h-48 dark:bg-gray-500 aspect-square'
-					src='https://source.unsplash.com/random/200x200/?7'
-				/>
-				<img
-					src='https://source.unsplash.com/random/302x302/'
+          src='https://lh3.googleusercontent.com/pw/AM-JKLWCEDgu85jeN0SjLvPUPHt9Njy3_ZlPTfrvAlQTLMu5rZ6tDFly20-zIDNLY7DdlOm1vb_jgJtHIcY7_MzhVE0WrE5ydnBJsBUPZINIrVZ9ACFQA6jkVTdnh2zvDoZ18L_0NdhJjmJg_4sza-NTxSjM9g=w1244-h933-no?authuser=0'
 					alt=''
 					className='w-full h-full col-span-2 row-span-2 rounded shadow-sm min-h-96 md:col-start-1 md:row-start-3 dark:bg-gray-500 aspect-square'
 				/>
