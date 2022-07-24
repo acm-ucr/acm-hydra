3b9c24d0cfda74cd6d60465d320c2dbaf0992121
Divyank Shah <divyank.shah.2016@gmail.com>
2022-07-24 00:29:27 -0700

    cleanup

--- pages/index.js
+++ pages/index.js
@@ -4,18 +4,17 @@ import Landing from "../components/Home/Landing";
 import Projects from "../components/Home/Projects";
 import Stats from "../components/Home/Stats";
 import Workshops from "../components/Home/Workshops";
-// import "bootstrap/dist/css/bootstrap.min.css";
 
 const IndexPage = () => {
-	return (
-		<div className=''>
-			<Landing />
-			<Stats />
-			<Workshops />
-			<Projects />
-			<Connect />
-		</div>
-	);
+    return (
+        <div className=''>
+            <Landing />
+            <Stats />
+            <Workshops />
+            <Projects />
+            <Connect />
+        </div>
+    );
 };
 
 export default IndexPage;
