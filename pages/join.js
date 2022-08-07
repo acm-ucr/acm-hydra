e29a8b0b40d0e38b508e5807413dffe7701eb1fa
Divyank Shah <divyank.shah.2016@gmail.com>
2022-08-06 18:09:05 -0700

    fix import

--- pages/join.js
+++ pages/join.js
@@ -1,16 +1,16 @@
 import React from "react";
 import Clubroom from "../components/Clubroom";
-import Faq from "../components/FAQ";
+import Faq from "../components/Faq";
 import StudentOrgs from "../components/StudentOrgs";
 
 const Join = () => {
-	return (
-		<div>
-			<Faq />
-			<Clubroom />
-			<StudentOrgs />
-		</div>
-	);
+    return (
+        <div>
+            <Faq />
+            <Clubroom />
+            <StudentOrgs />
+        </div>
+    );
 };
 
 export default Join;
