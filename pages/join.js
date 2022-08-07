9bb5575b313b59529d53488182458c082a91e918
Divyank Shah <divyank.shah.2016@gmail.com>
2022-08-06 18:15:46 -0700

    fix ci

--- pages/join.js
+++ pages/join.js
@@ -4,13 +4,13 @@ import Faq from "../components/Faq";
 import StudentOrgs from "../components/StudentOrgs";
 
 const Join = () => {
-    return (
-        <div>
-            <Faq />
-            <Clubroom />
-            <StudentOrgs />
-        </div>
-    );
+	return (
+		<div>
+			<Faq />
+			<Clubroom />
+			<StudentOrgs />
+		</div>
+	);
 };
 
 export default Join;
