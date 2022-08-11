8cfd064694dff242ee1c6cfb13be92e62b7636f9
Val <vwong031@ucr.edu>
2022-08-10 23:11:17 -0700

    stats added. fix formatting

--- components/Stats.js
+++ components/Stats.js
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Stats = () => {
-	return <div>Stats</div>;
+	return (
+		<div>
+			<ul className='text-5xl font-semibold flex items-center text-center space-x-20 leading-15'>
+				<li>10 members</li>
+				<li>10 alumni</li>
+				<li>10 socials</li>
+				<li>10 workshops</li>
+			</ul>
+		</div>
+	);
 };
 
 export default Stats;
