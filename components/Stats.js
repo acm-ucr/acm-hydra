4efbd4e21ebe88851f00442cd230d076767b03c4
Val <vwong031@ucr.edu>
2022-08-15 23:19:31 -0700

    stats component

--- components/Stats.js
+++ components/Stats.js
@@ -3,7 +3,7 @@ import React from "react";
 const Stats = () => {
 	return (
 		<div>
-			<ul className='text-5xl decoration-black font-semibold absolute bottom-0 leading-15 flex items-center text-center space-x-32'>
+			<ul className='font-lexend text-5xl decoration-black font-semibold absolute left-6 bottom-11 leading-15 flex items-center text-center space-x-32'>
 				<li>10 members</li>
 				<li>10 alumni</li>
 				<li>10 socials</li>
