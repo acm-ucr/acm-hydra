318271e311395c33776d77fcb2e7c24c6535aa36
Divyank Shah <divyank.shah.2016@gmail.com>
2022-08-15 17:38:38 -0700

    finalize changes

--- pages/_document.js
+++ pages/_document.js
@@ -1,21 +1,31 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-		<link rel="preconnect" href="https://fonts.googleapis.com"> 
-		<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin> 
-		<link href="https://fonts.googleapis.com/css2?family=Lexend&display=swap" rel="stylesheet">
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+	render() {
+		return (
+			<Html>
+				<Head>
+					<link
+						rel='preconnect'
+						href='https://fonts.googleapis.com'
+					/>
+					<link
+						rel='preconnect'
+						href='https://fonts.gstatic.com'
+						crossOrigin
+					/>
+					<link
+						href='https://fonts.googleapis.com/css2?family=Lexend&display=swap'
+						rel='stylesheet'
+					/>
+				</Head>
+				<body>
+					<Main />
+					<NextScript />
+				</body>
+			</Html>
+		);
+	}
 }
 
-export default MyDocument
+export default MyDocument;
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				lexend: ["Lexend"],
+			},
+		},
 	},
 	plugins: [
 		require("@tailwindcss/typography"),
