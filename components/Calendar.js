c606e9aa6f58e906a914bd987674e9051eff7a32
Divyank Shah <divyank.shah.2016@gmail.com>
2022-08-16 11:21:59 -0700

    implement filters

--- components/Calendar.js
+++ components/Calendar.js
@@ -1,7 +1,24 @@
 import React from "react";
+import Filter from "./Filter";
+import { Row, Col } from "react-bootstrap"
 
 const Calendar = () => {
-	return <div>Calendar</div>;
+    return <div>
+        <Row className="w-3/6">
+            <Col>
+                <Filter topic={"General Meeting"} />
+            </Col>
+            <Col>
+                <Filter topic={"Professional Development"} />
+            </Col>
+            <Col>
+                <Filter topic={"Socials"} />
+            </Col>
+            <Col>
+                <Filter topic={"Workshops"} />
+            </Col>
+        </Row>
+    </div>;
 };
 
 export default Calendar;
