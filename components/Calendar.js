c6354b1c3f6b3903b647ed870df7b9b3ac3a2bf5
theo <theobfernandez@gmail.com>
2022-08-17 16:28:53 -0700

    improve filter data through mapping

--- components/Calendar.js
+++ components/Calendar.js
@@ -2,22 +2,42 @@ import React from "react";
 import Filter from "./Filter";
 import { Row, Col } from "react-bootstrap";
 
+const filterMapping = [
+	{
+		topic: "Meetings",
+		color: "#82aaff",
+	},
+	{
+		topic: "Career",
+		color: "#c792ea",
+	},
+	{
+		topic: "Socials",
+		color: "#ff5370",
+	},
+	{
+		topic: "Workshops",
+		color: "#ffd700",
+	},
+];
+
 const Calendar = () => {
 	return (
 		<div>
 			<Row className='w-5/6 p-1'>
-				<Col xs={4} sm={3} lg={2} className='mb-1'>
-					<Filter topic={"Meetings"} color={"#82aaff"} />
-				</Col>
-				<Col xs={4} sm={3} lg={2} className='mb-1'>
-					<Filter topic={"Career"} color={"#c792ea"} />
-				</Col>
-				<Col xs={4} sm={3} lg={2} className='mb-1'>
-					<Filter topic={"Socials"} color={"#ff5370"} />
-				</Col>
-				<Col xs={4} sm={3} lg={2} className='mb-1'>
-					<Filter topic={"Workshops"} color={"#ffd700"} />
-				</Col>
+				{filterMapping.map((filter, index) => {
+					return (
+						<Col
+							key={index}
+							xs={4}
+							sm={3}
+							lg={2}
+							className='mb-1'
+						>
+							<Filter topic={filter.topic} color={filter.color} />
+						</Col>
+					);
+				})}
 			</Row>
 		</div>
 	);
