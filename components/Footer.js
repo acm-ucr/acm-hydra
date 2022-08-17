886f82d5c3cc6301054cf4c062b9fe1ec6224bab
Sarah Ramirez <sarrami270@gmail.com>
2022-08-16 17:12:44 -0700

    fixes spacing

--- components/Footer.js
+++ components/Footer.js
@@ -11,22 +11,21 @@ import {
 const Footer = () => {
 	return (
 		<footer className='bg-neutral-900 text-gray-50 py-6'>
-			<div className='container mx-auto'>
-				<div className='grid grid-rows-2 justify-center'>
+				<div className='grid sm:grid-rows-2 justify-center mx-auto'>
 					<div>
 						<p className='text-4xl font-lexend font-bold text-center'>
 							Let&#39;s connect!
 						</p>
 					</div>
 					<div>
-						<div className='grid grid-flow-col mx-auto gap-5'>
+						<div className='grid sm:grid-cols-6 grid-cols-3 justify-items-center mx-auto gap-4'>
 							<div>
 								<a
 									rel='noopener noreferrer'
 									href='https://www.instagram.com/acm_ucr/'
 									target='_blank'
 									title='Instagram'
-									className='mx-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-purple-500'
+									className='mx-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-fuchsia-400'
 								>
 									<FaInstagram size='56'></FaInstagram>
 								</a>
@@ -37,7 +36,7 @@ const Footer = () => {
 									href='https://docs.google.com/forms/d/e/1FAIpQLSfImoSRQ7d5lQASl5OPxxEK_2iiZT0UKxVsMsn3BMVCkqC-WQ/viewform'
 									target='_blank'
 									title='Discord'
-									className='m-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-indigo-500'
+									className='m-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-indigo-400'
 								>
 									<FaDiscord size='56'></FaDiscord>
 								</a>
@@ -70,7 +69,7 @@ const Footer = () => {
 									href='https://join.slack.com/t/csatucr/shared_invite/zt-6fz8g1lu-oKHsfL1qx3wZMJ9k3j2sXw'
 									target='_blank'
 									title='Slack'
-									className='m-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-gray-300'
+									className='m-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-sky-500'
 								>
 									<FaSlack size='56'></FaSlack>
 								</a>
@@ -81,7 +80,7 @@ const Footer = () => {
 									href='https://medium.com/acm-at-ucr'
 									target='_blank'
 									title='Medium'
-									className='m-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-gray-300'
+									className='m-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-gray-400'
 								>
 									<FaMediumM size='56'></FaMediumM>
 								</a>
@@ -89,7 +88,6 @@ const Footer = () => {
 						</div>
 					</div>
 				</div>
-			</div>
 		</footer>
 	);
 };
