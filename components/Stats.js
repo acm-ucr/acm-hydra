aaf00bd8f431c103b3da48e483c060727cc46c78
Val <vwong031@ucr.edu>
2022-08-17 18:18:16 -0700

    fixed formatting. still need to fix positioning & responsiveness

--- components/Stats.js
+++ components/Stats.js
@@ -1,16 +1,25 @@
 import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
 
 const Stats = () => {
 	return (
-		<div className='container'>
-			<div className='row'>
-				<div className='font-lexend text-5xl decoration-black font-semibold flex align-items-center text-center'>
-					<div className='col'>10+ members</div>
-					<div className='col'>10 alumni</div>
-					<div className='col'>10 socials</div>
-					<div className='col'>10 workshops</div>
-				</div>
-			</div>
+		<div>
+			<Container className='justify-center mx-auto'>
+				<Row className='font-lexend text-5xl decoration-black font-semibold justify-center mx-auto text-center'>
+					<Col xs={4} md={2} className='flex justify-center'>
+						10+ members
+					</Col>
+					<Col xs={4} md={2} className='flex justify-center'>
+						10 alumni
+					</Col>
+					<Col xs={4} md={2} className='flex justify-center'>
+						10 socials
+					</Col>
+					<Col xs={4} md={2} className='flex justify-center'>
+						10 workshops
+					</Col>
+				</Row>
+			</Container>
 		</div>
 	);
 };
