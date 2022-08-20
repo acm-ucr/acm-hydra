12fa9c3026bc2611ad857df9683325863bc199a5
Raidah Fairooz <raidahfairooz@Raidahs-MacBook-Pro.local>
2022-08-20 03:10:09 -0400

    added bootstrap grid in Learn.js and created a basic structure of the Learn slide

--- components/Learn.js
+++ components/Learn.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 
 const Learn = () => {
-	return <div>Learn</div>;
+	return (
+		<div className='font-Arial text-lg'>
+			{" "}
+			<div className='flex justify-center items-center font-Arial font-bold text-4xl mb-4'>
+				Learn
+			</div>
+			<Row>
+				<Col className='flex justify-around'>professional</Col>
+				<Col className='flex justify-center items-center'>
+					technical
+				</Col>
+				<Col className='flex justify-around'>academic</Col>
+			</Row>
+		</div>
+	);
 };
 
 export default Learn;
