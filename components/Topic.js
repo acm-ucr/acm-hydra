3359ed495e31584c3a02e66bf15ed804494549f8
Raidah Fairooz <raidahfairooz@Raidahs-MacBook-Pro.local>
2022-08-20 02:34:43 -0400

    created topic.js, included in events page

--- components/Topic.js
+++ components/Topic.js
@@ -1,6 +1,16 @@
 import React from "react";
 
 const Topic = () => {
+	// {
+	// 	Topic, Color;
+	// } -> take in as props
+	// <div className="make the box here" size not pixels
+	// <div className="properties of the text">
+	// 	{Topic}
+	// </div>
+	// <div className="set the box background color">
+	// 	{color}
+	// </div>
 	return <div>Topic</div>;
 };
 
--- pages/events.js
+++ pages/events.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Calendar from "../components/Calendar";
 import PastEvents from "../components/PastEvents";
+import Topic from "../components/Topic";
 
 const Events = () => {
 	return (
 		<div>
 			<Calendar />
 			<PastEvents />
+			<Topic />
 		</div>
 	);
 };
