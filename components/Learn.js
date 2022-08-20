531c11bb44564dd69c6cd17fcc056d9bb9117e2d
Raidah Fairooz <raidahfairooz@Raidahs-MacBook-Pro.local>
2022-08-20 03:28:02 -0400

    added topic components int he learn grid, fixed pr1 issues

--- components/Learn.js
+++ components/Learn.js
@@ -1,19 +1,27 @@
 import React from "react";
 import { Row, Col } from "react-bootstrap";
+import Topic from "../components/Topic";
 
 const Learn = () => {
 	return (
-		<div className='font-Arial text-lg'>
+		<div className='font-lexend text-lg'>
 			{" "}
-			<div className='flex justify-center items-center font-Arial font-bold text-4xl mb-4'>
+			<div className='flex justify-center items-center font-lexend font-bold text-4xl mb-4'>
 				Learn
 			</div>
 			<Row>
-				<Col className='flex justify-around'>professional</Col>
+				<Col className='flex justify-around'>
+					professional
+					<Topic />
+				</Col>
 				<Col className='flex justify-center items-center'>
 					technical
+					<Topic />
 				</Col>
-				<Col className='flex justify-around'>academic</Col>
+				<Col className='flex justify-around'>
+					academic
+					<Topic />
+				</Col>
 			</Row>
 		</div>
 	);
--- pages/events.js
+++ pages/events.js
@@ -1,14 +1,12 @@
 import React from "react";
 import Calendar from "../components/Calendar";
 import PastEvents from "../components/PastEvents";
-import Topic from "../components/Topic";
 
 const Events = () => {
 	return (
 		<div>
 			<Calendar />
 			<PastEvents />
-			<Topic />
 		</div>
 	);
 };
