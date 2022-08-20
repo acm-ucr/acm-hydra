feba64eda7010c5f00178eac6028ddd78f7901d0
Val <vwong031@ucr.edu>
2022-08-19 22:01:46 -0700

    fix responsiveness

--- components/Stats.js
+++ components/Stats.js
@@ -4,18 +4,18 @@ import { Row, Col, Container } from "react-bootstrap";
 const Stats = () => {
 	return (
 		<div>
-			<Container className='justify-center mx-auto'>
-				<Row className='font-lexend text-5xl decoration-black font-semibold justify-center mx-auto text-center'>
-					<Col xs={4} md={2} className='flex justify-center'>
+			<Container>
+				<Row className='font-lexend text-4xl decoration-black font-semibold'>
+					<Col xs={4} md={3} className='flex justify-center'>
 						10+ members
 					</Col>
-					<Col xs={4} md={2} className='flex justify-center'>
+					<Col xs={4} md={3} className='flex justify-center'>
 						10 alumni
 					</Col>
-					<Col xs={4} md={2} className='flex justify-center'>
+					<Col xs={4} md={3} className='flex justify-center'>
 						10 socials
 					</Col>
-					<Col xs={4} md={2} className='flex justify-center'>
+					<Col xs={4} md={3} className='flex justify-center'>
 						10 workshops
 					</Col>
 				</Row>
