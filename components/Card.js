ea377cbdae6b2de7e1cfc4c82e936fb19b75995f
Kavin Phabiani <kavinphab@gmail.com>
2022-08-21 00:14:16 -0700

    resolved messgaes

--- components/Card.js
+++ components/Card.js
@@ -1,45 +1,43 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
 
-class Block extends React.Component {
-	render() {
-		return (
-			// have one shadow white and one black right behind it looking like a border
-			<div className='flex justify-center'>
-				<Card
-					style={{
-						width: "15rem",
-						backgroundColor: "black",
-						borderRadius: 25,
-						boxShadow: "12px -13px white, 16px -17px black",
-					}}
-				>
-					<Card.Body>
-						<div className='flex justify-evenly '>
-							<div className='columns-1 text-blue-500'>
-								<Card.Title>09</Card.Title>
-								<Card.Title>21</Card.Title>
-								<Card.Title>22</Card.Title>
-							</div>
+const Block = ({ month, day, year, type, time, place, description }) => {
+	return (
+		// have one shadow white and one black right behind it looking like a border
+		<div className='flex justify-center'>
+			<Card
+				style={{
+					width: "15rem",
+					backgroundColor: "black",
+					borderRadius: 25,
+					boxShadow: "12px -13px white, 16px -17px black",
+				}}
+			>
+				<Card.Body>
+					<div className='flex justify-evenly '>
+						<div className='columns-1 text-blue-500'>
+							<Card.Title>{month}</Card.Title>
+							<Card.Title>{day}</Card.Title>
+							<Card.Title>{year}</Card.Title>
+						</div>
 
-							<div className='columns-1'>
-								<div className='text-blue-500'>
-									<Card.Title>General Meeting</Card.Title>
-								</div>
-								<div className='text-white'>
-									<Card.Title>5-6 pm</Card.Title>
-									<Card.Subtitle>WCH 127</Card.Subtitle>
-									<br />
-									<Card.Text>blah blah</Card.Text>
-								</div>
+						<div className='columns-1'>
+							<div className='text-blue-500 text-xlg'>
+								<Card.Title>{type}</Card.Title>
+							</div>
+							<div className='text-white'>
+								<Card.Title>{time}</Card.Title>
+								<Card.Subtitle>{place}</Card.Subtitle>
+								<br />
+								<Card.Text>{description}</Card.Text>
 							</div>
 						</div>
-					</Card.Body>
-				</Card>
-			</div>
-		);
-	}
-}
+					</div>
+				</Card.Body>
+			</Card>
+		</div>
+	);
+};
 
 {
 }
