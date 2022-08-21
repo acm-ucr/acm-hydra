b416f7e6abfaa41853bd9627dadf301aff49cbf3
Val <vwong031@ucr.edu>
2022-08-21 16:19:29 -0700

    fixed title, text, & image into a row

--- components/Introduction.js
+++ components/Introduction.js
@@ -4,17 +4,16 @@ import { Row, Col, Container } from "react-bootstrap";
 const Introduction = () => {
 	return (
 		<Container>
-			<Col className='font-lexend'>
+			<Row className='font-lexend flex justify-start text-center items-center'>
 				<Row className='text-7xl font-semibold'>We are ACM @ UCR</Row>
-				<Row className='text-5xl font-normal'>
+				<Row className='text-5xl font-normal w-75'>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					Arcu, amet ut orci, at sed parturient quis sagittis a.
 				</Row>
-			</Col>
-      
-			<Col>
-				<img className='' alt='' src='./acm-ucr-logo.png' />
-			</Col>
+				<Col>
+					<img className='w-95' alt='' src='./acm-ucr-logo.png' />
+				</Col>
+			</Row>
 		</Container>
 	);
 };
