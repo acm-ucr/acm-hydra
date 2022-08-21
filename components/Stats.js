600d896a39d6d4af8459a38dd9c4c41e6e7376ae
Val <vwong031@ucr.edu>
2022-08-20 19:34:19 -0700

    fix formatting

--- components/Stats.js
+++ components/Stats.js
@@ -3,42 +3,42 @@ import { Row, Col, Container } from "react-bootstrap";
 
 const Stats = () => {
 	return (
-    <Container>
-      <Row className='font-lexend text-5xl decoration-black font-semibold flex justify-between items-center whitespace-nowrap'>
-        <Col
-          sm={12}
-          md={6}
-          lg={3}
-          className='flex justify-center whitespace-nowrap'
-        >
-          10+ members
-        </Col>
-        <Col
-          sm={12}
-          md={6}
-          lg={3}
-          className='flex justify-center whitespace-nowrap'
-        >
-          10 alumni
-        </Col>
-        <Col
-          sm={12}
-          md={6}
-          lg={3}
-          className='flex justify-center whitespace-nowrap'
-        >
-          10 socials
-        </Col>
-        <Col
-          sm={12}
-          md={6}
-          lg={3}
-          className='flex justify-center whitespace-nowrap'
-        >
-          10 workshops
-        </Col>
-      </Row>
-    </Container>
+		<Container>
+			<Row className='font-lexend text-5xl decoration-black font-semibold flex justify-between items-center whitespace-nowrap'>
+				<Col
+					sm={12}
+					md={6}
+					lg={3}
+					className='flex justify-center whitespace-nowrap'
+				>
+					10+ members
+				</Col>
+				<Col
+					sm={12}
+					md={6}
+					lg={3}
+					className='flex justify-center whitespace-nowrap'
+				>
+					10 alumni
+				</Col>
+				<Col
+					sm={12}
+					md={6}
+					lg={3}
+					className='flex justify-center whitespace-nowrap'
+				>
+					10 socials
+				</Col>
+				<Col
+					sm={12}
+					md={6}
+					lg={3}
+					className='flex justify-center whitespace-nowrap'
+				>
+					10 workshops
+				</Col>
+			</Row>
+		</Container>
 	);
 };
 
