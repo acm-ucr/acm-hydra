6f3b94ff053c968d9ab89c20a4aa85454a096bfe
Val <vwong031@ucr.edu>
2022-08-21 15:43:19 -0700

    title, text, & image added. fix sizing, responsiveness, etc

--- components/Introduction.js
+++ components/Introduction.js
@@ -1,7 +1,22 @@
 import React from "react";
+import { Row, Col, Container } from "react-bootstrap";
 
 const Introduction = () => {
-	return <div>Introduction</div>;
+	return (
+		<Container>
+			<Col className='font-lexend'>
+				<Row className='text-7xl font-semibold'>We are ACM @ UCR</Row>
+				<Row className='text-5xl font-normal'>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Arcu, amet ut orci, at sed parturient quis sagittis a.
+				</Row>
+			</Col>
+      
+			<Col>
+				<img className='' alt='' src='./acm-ucr-logo.png' />
+			</Col>
+		</Container>
+	);
 };
 
 export default Introduction;
