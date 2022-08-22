01eae0fff5d90c3ef4421b5b9825d9d5fa179b92
Val <vwong031@ucr.edu>
2022-08-21 20:00:54 -0700

    centered title & text. must fix logo

--- components/Introduction.js
+++ components/Introduction.js
@@ -4,14 +4,30 @@ import { Row, Col, Container } from "react-bootstrap";
 const Introduction = () => {
 	return (
 		<Container>
-			<Row className='font-lexend flex justify-start text-center items-center'>
-				<Row className='text-7xl font-semibold'>We are ACM @ UCR</Row>
-				<Row className='text-5xl font-normal w-75'>
+			<Row className='font-lexend flex justify-start text-center items-center m-auto'>
+				<Row
+					sm={3}
+					md={3}
+					lg={3}
+					className='text-7xl font-semibold flex justify-center w-75'
+				>
+					We are ACM @ UCR
+				</Row>
+				<Row
+					sm={3}
+					md={3}
+					lg={3}
+					className='text-5xl font-normal flex justify-center w-75'
+				>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					Arcu, amet ut orci, at sed parturient quis sagittis a.
 				</Row>
-				<Col>
-					<img className='w-95' alt='' src='./acm-ucr-logo.png' />
+				<Col sm={3} md={3} lg={3} className=''>
+					<img
+						className=''
+						alt='acm @ ucr logo'
+						src='./acm-ucr-logo.png'
+					/>
 				</Col>
 			</Row>
 		</Container>
