1899b93909eb6b78504c8b0f0c3eb453c8f8fbef
Divyank Shah <divyank.shah.2016@gmail.com>
2022-08-22 01:50:01 -0700

    add tailwind colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,24 @@ module.exports = {
 		"./components/**/*.{js,ts,jsx,tsx}",
 	],
 	theme: {
+		colors: {
+			acm: {
+				blue: "#82aaff",
+				lightpurple: "#c792ea",
+				yellow: "#ffd700",
+				green: "#c3e88d",
+				red: "#ff5370",
+				gray: "#8f93a2",
+				pink: "#f07178",
+				lightblue: "#89ddff",
+				darkgray: "#525975",
+				orange: "#f78c6c",
+				marine: "#80cbc4",
+				purple: "#da70d6",
+				white: "#ffffff",
+				black: "#000000",
+			},
+		},
 		extend: {
 			fontFamily: {
 				lexend: ["Lexend"],
