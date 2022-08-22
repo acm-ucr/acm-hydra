7e05052bfb809aaa7b5ce88b57652b3a00fb3bf4
Aarav Sane <aaravtennis@gmail.com>
2022-08-22 19:57:32 +0530

    code cleanup and small fixes

--- components/Navigation.js
+++ components/Navigation.js
@@ -1,39 +1,20 @@
 import React from "react";
 import { Nav, Navbar } from "react-bootstrap";
-// import About from "../pages/about.js";
-// import Events from "../pages/events.js";
-// import Board from "../pages/board.js";
-// import Join from "../pages/join.js";
-// import IndexPage from "../pages/index.js";
 import Link from "next/link";
-
-// const LinkPage = pages => {
-// 	if (pages == "about") {
-// 		return <About />;
-// 	} else if (pages == "events") {
-// 		return <Events />;
-// 	} else if (pages == "board") {
-// 		return <Board />;
-// 	} else if (pages == "joinus") {
-// 		return <Join />;
-// 	} else if (pages == "home") {
-// 		return <IndexPage />;
-// 	}
-// };
+import Image from "next/image";
 
 const Navigation = () => {
-	// const [expanded, setExpanded] = useState(false);
 	return (
 		<Navbar collapseOnSelect bg='light' expand='lg'>
 			<Navbar.Brand>
 				<Link href='/' passHref>
 					<Nav.Link eventKey='1'>
-						<img
-							src='public/acmlogo.png'
+						<Image
+							src='/../public/acmlogo.png'
 							alt=''
-							width='70'
-							height='70'
-						></img>
+							width='70%'
+							height='70%'
+						/>
 					</Nav.Link>
 				</Link>
 			</Navbar.Brand>
@@ -75,7 +56,7 @@ const Navigation = () => {
 							</Link>
 						</li>
 						<li className='nav-item active nav-link font-lexend font-color-black'>
-							<Link align='right' href='/joinus' passHref>
+							<Link align='right' href='/join' passHref>
 								<Nav.Link
 									className='text-black no-underline'
 									eventKey='5'
@@ -90,31 +71,5 @@ const Navigation = () => {
 		</Navbar>
 	);
 };
-
-{
-	/* <div>
-			<nav className='navbar navbar-expand-lg navbar-light bg-light fs-5 font-lexend'>
-				<div className='container-fluid'>
-					<a className='navbar-brand' href='/'>
-						<LinkPage page='home' />
-					</a>
-					<button
-						className='navbar-toggler'
-						type='button'
-						data-bs-toggle='dropdown'
-						data-bs-target='#navbarToggler'
-					>
-						<span className='navbar-toggler-icon'></span>
-					</button>
-					<div
-						className='navbar-collapse collapse'
-						id='navbarToggler'
-					></div>
-				</div>
-			</nav>
-		</div>
-	);
-}; */
-}
 
 export default Navigation;
