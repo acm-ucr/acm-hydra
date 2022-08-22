2e4585d94dfe48ee69c9777bd74ec4199bcc6c7d
Val <vwong031@ucr.edu>
2022-08-22 15:24:18 -0700

    fix title responsiveness

--- components/Introduction.js
+++ components/Introduction.js
@@ -3,21 +3,18 @@ import { Row, Col } from "react-bootstrap";
 
 const Introduction = () => {
 	return (
-		<Row className='font-lexend flex justify-center text-center items-center'>
-			<Col sm={3} md={3} lg={3} className='w-5/6'>
-				<p className='text-7xl font-semibold flex justify-center'>
+		<Row className='font-lexend flex justify-center text-center w-11/12 items-center mx-auto'>
+			<Col>
+				<Row className='text-7xl font-semibold flex justify-center mx-auto whitespace-nowrap'>
 					We are ACM @ UCR
-				</p>
-				<p className='text-5xl font-normal flex justify-center'>
+				</Row>
+				<Row className='text-5xl font-normal flex justify-center mx-auto w-5/6 pt-4'>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					Arcu, amet ut orci, at sed parturient quis sagittis a.
-				</p>
+				</Row>
 			</Col>
-			<Col sm={3} md={3} lg={3}>
-				<img
-					alt='acm @ ucr logo'
-					src='acm-ucr-logo.png'
-				/>
+			<Col>
+				<img alt='acm @ ucr logo' src='acm-ucr-logo.png' />
 			</Col>
 		</Row>
 	);
