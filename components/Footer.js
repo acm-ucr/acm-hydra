ee27808367c9ecd3cf5ba4360264a602a4c5b978
Sarah Ramirez <sarrami270@gmail.com>
2022-08-23 16:57:11 -0700

    footer mapping done

--- components/Footer.js
+++ components/Footer.js
@@ -9,6 +9,53 @@ import {
 	FaGithub,
 } from "react-icons/fa";
 
+const SocialLinks = [
+	{
+		path: "https://www.instagram.com/acm_ucr/",
+		title: "Instagram",
+		image: <FaInstagram size='56'></FaInstagram>,
+	},
+	{
+		path: "https://docs.google.com/forms/d/e/1FAIpQLSfImoSRQ7d5lQASl5OPxxEK_2iiZT0UKxVsMsn3BMVCkqC-WQ/viewform",
+		title: " Discord",
+		image: <FaDiscord size='56'></FaDiscord>,
+	},
+	{
+		path: "https://www.youtube.com/channel/UCSLoGcSzNfpHIzdT6QzsmnQ",
+		title: "Youtube",
+		image: <FaYoutube size='56'></FaYoutube>,
+	},
+	{
+		path: "https://github.com/acm-ucr",
+		title: "Github",
+		image: <FaGithub size='56'></FaGithub>,
+	},
+	{
+		path: "https://join.slack.com/t/csatucr/shared_invite/zt-6fz8g1lu-oKHsfL1qx3wZMJ9k3j2sXw",
+		title: "Slack",
+		image: <FaSlack size='56'></FaSlack>,
+	},
+	{
+		path: "https://medium.com/acm-at-ucr",
+		title: "Medium",
+		image: <FaMediumM size='56'></FaMediumM>,
+	},
+];
+
+const FooterLink = ({ path, title, image }) => {
+	return (
+		<a
+			rel='noopener noreferrer'
+			href={path}
+			target='_blank'
+			title={title}
+			className='mx-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-gray-300'
+		>
+			{image}
+		</a>
+	);
+};
+
 const Footer = () => {
 	return (
 		<footer className='bg-neutral-900 text-gray-50 py-6'>
@@ -19,72 +66,14 @@ const Footer = () => {
 					</p>
 				</Row>
 				<Row className='justify-center mx-auto'>
-					<Col xs={4} md={2} className='flex justify-center'>
-						<a
-							rel='noopener noreferrer'
-							href='https://www.instagram.com/acm_ucr/'
-							target='_blank'
-							title='Instagram'
-							className='mx-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-gray-300'
-						>
-							<FaInstagram size='56'></FaInstagram>
-						</a>
-					</Col>
-					<Col xs={4} md={2} className='flex justify-center'>
-						<a
-							rel='noopener noreferrer'
-							href='https://docs.google.com/forms/d/e/1FAIpQLSfImoSRQ7d5lQASl5OPxxEK_2iiZT0UKxVsMsn3BMVCkqC-WQ/viewform'
-							target='_blank'
-							title='Discord'
-							className='m-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-gray-300'
-						>
-							<FaDiscord size='56'></FaDiscord>
-						</a>
-					</Col>
-					<Col xs={4} md={2} className='flex justify-center'>
-						<a
-							rel='noopener noreferrer'
-							href='https://www.youtube.com/channel/UCSLoGcSzNfpHIzdT6QzsmnQ'
-							target='_blank'
-							title='Youtube'
-							className='m-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-gray-300'
-						>
-							<FaYoutube size='56'></FaYoutube>
-						</a>
-					</Col>
-					<Col xs={4} md={2} className='flex justify-center'>
-						<a
-							rel='noopener noreferrer'
-							href='https://github.com/acm-ucr'
-							target='_blank'
-							title='Github'
-							className='m-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-gray-300'
-						>
-							<FaGithub size='56'></FaGithub>
-						</a>
-					</Col>
-					<Col xs={4} md={2} className='flex justify-center'>
-						<a
-							rel='noopener noreferrer'
-							href='https://join.slack.com/t/csatucr/shared_invite/zt-6fz8g1lu-oKHsfL1qx3wZMJ9k3j2sXw'
-							target='_blank'
-							title='Slack'
-							className='m-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-gray-300'
-						>
-							<FaSlack size='56'></FaSlack>
-						</a>
-					</Col>
-					<Col xs={4} md={2} className='flex justify-center'>
-						<a
-							rel='noopener noreferrer'
-							href='https://medium.com/acm-at-ucr'
-							target='_blank'
-							title='Medium'
-							className='m-auto fill-current md:mx-12 lg:m-0 text-gray-50 hover:text-gray-300'
-						>
-							<FaMediumM size='56'></FaMediumM>
-						</a>
-					</Col>
+						{SocialLinks.map((link, index) => {
+							return (
+								<Col key={index} xs={4} md={2} className='flex justify-center'>	
+									<FooterLink {...link}/>
+								</Col>
+							);
+
+						})}
 				</Row>
 			</Container>
 		</footer>
