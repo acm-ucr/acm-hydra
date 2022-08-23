ea1aa8699d6f55d8992e74c2c96d7977971c00f5
Val <vwong031@ucr.edu>
2022-08-22 23:52:32 -0700

    need to get it to work for mobile/small screen

--- components/Introduction.js
+++ components/Introduction.js
@@ -3,12 +3,12 @@ import { Row, Col } from "react-bootstrap";
 
 const Introduction = () => {
 	return (
-		<Row className='font-lexend flex justify-center text-center w-11/12 items-center mx-auto'>
-			<Col>
+		<Row className='font-lexend flex justify-between items-center w-11/12 mx-auto'>
+			<Col className='text-center'>
 				<Row className='text-7xl font-semibold flex justify-center mx-auto whitespace-nowrap'>
 					We are ACM @ UCR
 				</Row>
-				<Row className='text-5xl font-normal flex justify-center mx-auto w-5/6 pt-4'>
+				<Row className='text-5xl font-normal flex justify-center mx-auto w-5/6 pt-5'>
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
 					Arcu, amet ut orci, at sed parturient quis sagittis a.
 				</Row>
