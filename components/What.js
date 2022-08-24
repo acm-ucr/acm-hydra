a5757e0f784ec67493be884479d9446c94e879b5
Val <vwong031@ucr.edu>
2022-08-24 14:57:50 -0700

    image, title, & text added

--- components/What.js
+++ components/What.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 
 const What = () => {
-	return <div>What</div>;
+	return (
+		<Row className='font-lexend flex justify-between items-center w-5/6 mx-auto'>
+			<Col sm={6}>
+				<img alt='picture frame component' src='' />
+			</Col>
+			<Col sm={6} className='text-center'>
+				<p className='text-6xl font-medium flex justify-center mx-auto'>
+					What we do!
+				</p>
+				<p className='text-4xl font-normal flex justify-center mx-auto w-11/12 pt-3'>
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+					Consequat vel ultrices neque odio egestas tempor id. Semper
+					tortor, eget nisi vivamus. Cras aliquam, maecenas facilisis
+					tempor massa dui tempus eget. Eros consequat nullam
+					tristique tristique placerat cursus amet integer. Ac nunc,
+					at lobortis phasellus eu dui. Vitae consectetur diam sed
+					venenatis non. Eleifend varius volutpat sapien tellus tortor
+					ultrices. Semper.
+				</p>
+			</Col>
+		</Row>
+	);
 };
 
 export default What;
