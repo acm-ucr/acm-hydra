e45f3cdc77b0d2ea0232081b65a4ee16239480f7
Val <vwong031@ucr.edu>
2022-08-24 15:28:45 -0700

    replaced text & changed width

--- components/Why.js
+++ components/Why.js
@@ -3,20 +3,21 @@ import { Row, Col } from "react-bootstrap";
 
 const Why = () => {
 	return (
-		<Row className='font-lexend flex justify-between items-center w-5/6 mx-auto'>
+		<Row className='font-lexend flex justify-between items-center w-11/12 mx-auto'>
 			<Col sm={6} className='text-center'>
 				<p className='text-6xl font-medium flex justify-center mx-auto'>
 					Why we do it!
 				</p>
 				<p className='text-4xl font-normal flex justify-center mx-auto w-11/12 pt-3'>
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-					Pharetra cursus mauris dictum euismod amet in enim
-					adipiscing aenean. Sagittis aenean leo praesent ut vulputate
-					duis dolor. Commodo ut odio dignissim tellus convallis magna
-					ac auctor. Cursus urna pellentesque convallis tristique.
-					Risus dictum feugiat fusce habitasse. Imperdiet risus dui,
-					nunc tortor nisl id aliquet urna rhoncus. Tellus nunc
-					curabitur mi.
+					We want UCR to become a premier institution for Computer
+					Science and Engineering. University courses provide a good
+					foundation, but we strive to build upon that. So we
+					supplement the curriculum with workshops tailored to our
+					members and their goals. We do this through an amazing 1:1
+					Mentor program, and constant feedback from our members. But
+					more importantly, we want our members to make a positive
+					difference in the world through software. The success of ACM
+					at UCR can only be measured by the success of our members.
 				</p>
 			</Col>
 			<Col sm={6}>
