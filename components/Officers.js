ae2e2d103ecef32f4bab9c87408389a07927ddc1
Kavin Phabiani <kavinphab@gmail.com>
2022-08-26 17:26:13 -0700

    Setup basic structure

--- components/Officers.js
+++ components/Officers.js
@@ -1,7 +1,58 @@
 import React from "react";
+import Profile from "./Profile";
+import { Row, Col } from "react-bootstrap";
 
 const Officers = () => {
-	return <div>Officers</div>;
+	return (
+		<div>
+			<text className='flex justify-center text-5xl font-bold'>
+				Upcoming Events
+			</text>
+			<br />
+			<Row>
+				<Col>
+					<Profile
+						month='01'
+						day='02'
+						year='03'
+						image='general'
+						name='3'
+						place='WCH'
+					/>
+				</Col>
+				<Col>
+					<Profile
+						month='01'
+						day='02'
+						year='03'
+						image='general'
+						name='3'
+						place='WCH'
+					/>
+				</Col>
+				<Col>
+					<Profile
+						month='01'
+						day='02'
+						year='03'
+						image='general'
+						name='3'
+						place='WCH'
+					/>
+				</Col>
+				<Col>
+					<Profile
+						month='01'
+						day='02'
+						year='03'
+						image='general'
+						name='3'
+						place='WCH'
+					/>
+				</Col>
+			</Row>
+		</div>
+	);
 };
 
 export default Officers;
--- components/Profile.js
+++ components/Profile.js
@@ -1,7 +1,35 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Card from "react-bootstrap/Card";
 
-const Profile = () => {
-	return <div>Profile</div>;
+const Profile = ({ month, day, year, image, name, place }) => {
+	return (
+		<Card className='bg-blue-300 font-lexend'>
+			<Card.Body className='flex justify-evenly'>
+				<Card.Title className='w-1/6'>
+					<p className='text-3xl inline text-white'>
+						{month}
+						<br />
+						{day}
+						<br />
+						{year}
+					</p>
+				</Card.Title>
+				<div className='w-4/6'>
+					<Card.Title>
+						<p className='text-3xl inline text-white'>{image}</p>
+					</Card.Title>
+					<div className='text-white'>
+						<Card.Text>
+							{name}
+							<br />
+							{place}
+						</Card.Text>
+					</div>
+				</div>
+			</Card.Body>
+		</Card>
+	);
 };
 
 export default Profile;
