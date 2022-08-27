ee61cbd483247d0781eef8402c2c26257ad094c1
Val <vwong031@ucr.edu>
2022-08-26 19:24:51 -0700

    basic setup & calling gallery component from about page

---
+++ components/Gallery.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+const Gallery = () => {
+  return (
+    <Row className='flex justify-center mx-auto'>
+      <Row className='font-lexend text-7xl font-semibold flex justify-center mx-auto p-4'>
+        Gallery
+      </Row>
+      <Row>
+        <Row>
+          <Col>
+
+          </Col>
+          <Col>
+
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+
+          </Col>
+          <Col>
+          
+          </Col>
+        </Row>
+        <Row>
+
+        </Row>
+      </Row>
+    </Row>
+  );
+};
+
+export default Gallery;
