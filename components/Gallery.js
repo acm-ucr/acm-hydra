4208af82760f641b76b5f744ab71d2f21b80c68b
Val <vwong031@ucr.edu>
2022-08-26 22:42:16 -0700

    fixed formatting

--- components/Gallery.js
+++ components/Gallery.js
@@ -50,7 +50,11 @@ const Gallery = () => {
 								alt='students sitting at tables listening to a presentation'
 								src='image15.png'
 							/>
-							<img className='p-1' alt='students in a large classroom watching a presentation' src='image14.png' />
+							<img
+								className='p-1'
+								alt='students in a large classroom watching a presentation'
+								src='image14.png'
+							/>
 						</Col>
 						<Col>
 							<img
@@ -58,15 +62,26 @@ const Gallery = () => {
 								alt='students watching a presentation on development workflow'
 								src='image17.png'
 							/>
-							<img className='p-1' alt='students following along on their laptops in an acm workshop' src='image9.jpg' />
+							<img
+								className='p-1'
+								alt='students following along on their laptops in an acm workshop'
+								src='image9.jpg'
+							/>
 						</Col>
 					</Col>
 					<Col className='py-2'>
-						<img alt='students sitting at tables discussing their ideas with professors' src='image10.jpg' />
+						<img
+							alt='students sitting at tables discussing their ideas with professors'
+							src='image10.jpg'
+						/>
 					</Col>
 				</Row>
 				<Row>
-					<img className='pt-2 pb-16' alt='students paying attention during an acm meeting' src='image12.png' />
+					<img
+						className='pt-2 pb-16'
+						alt='students paying attention during an acm meeting'
+						src='image12.png'
+					/>
 				</Row>
 			</Row>
 		</Row>
