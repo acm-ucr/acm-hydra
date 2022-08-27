a19281d20bcdfdfe32f272f25be876e7e5f59ae4
Val <vwong031@ucr.edu>
2022-08-27 00:35:00 -0700

    changed title & text size

--- components/Vision.js
+++ components/Vision.js
@@ -6,10 +6,10 @@ const Vision = () => {
 	return (
 		<Row className='font-lexend flex justify-between items-center w-11/12 mx-auto'>
 			<Col sm={6} className='text-center'>
-				<p className='text-6xl font-semibold flex justify-center mx-auto'>
+				<p className='text-3xl font-semibold flex justify-center mx-auto'>
 					Our Vision
 				</p>
-				<p className='text-4xl font-normal flex justify-center mx-auto w-11/12 pt-3'>
+				<p className='text-xl font-normal flex justify-center mx-auto w-11/12 pt-3'>
 					Our Vision is simple: We want to improve our members
 					professionally, technically, and personally. We want to help
 					our members grow throughout their college career, whether
--- components/Why.js
+++ components/Why.js
@@ -6,10 +6,10 @@ const Why = () => {
 	return (
 		<Row className='font-lexend flex justify-between items-center w-11/12 mx-auto'>
 			<Col sm={6} className='text-center'>
-				<p className='text-6xl font-medium flex justify-center mx-auto'>
+				<p className='text-3xl font-medium flex justify-center mx-auto'>
 					Why we do it!
 				</p>
-				<p className='text-4xl font-normal flex justify-center mx-auto w-11/12 pt-3'>
+				<p className='text-xl font-normal flex justify-center mx-auto w-11/12 pt-3'>
 					We want UCR to become a premier institution for Computer
 					Science and Engineering. University courses provide a good
 					foundation, but we strive to build upon that. So we
