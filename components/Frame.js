d2a7a10dda8721638eb6f9aab80ee36034bc435c
Tony <tonytrieu03@outlook.com>
2022-08-26 23:27:38 -0700

    The "blank square" is the blank img from Figma

--- components/Frame.js
+++ components/Frame.js
@@ -1,7 +1,21 @@
 import React from "react";
+import Image from "./BlankSquare.png";
 
 const Frame = () => {
-	return <div>Frame</div>;
+	const frameStyle = {
+		borderImage:
+			"conic-gradient(from 10deg, marine 70deg, black 0deg, black 175deg, lightpurple 175deg, lightpurple 255deg, black 3deg)",
+		borderImageSlice: 1,
+	};
+
+	return (
+		<img
+			className='ml-5 border-solid'
+			src={Image}
+			style={frameStyle}
+			alt='Square image'
+		/>
+	);
 };
 
 export default Frame;
