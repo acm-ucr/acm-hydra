00e124f4e596be7ff10b149f1935fdc26867c648
Val <vwong031@ucr.edu>
2022-08-26 22:41:53 -0700

    added alt text

--- components/Gallery.js
+++ components/Gallery.js
@@ -10,36 +10,63 @@ const Gallery = () => {
 			<Row className='flex justify-center m-auto w-11/12'>
 				<Row>
 					<Col className='py-2'>
-						<img alt='' src='image11.png' />
+						<img
+							alt='group of students gathering in front of rivera library'
+							src='image11.png'
+						/>
 					</Col>
 					<Col className='flex justify-center m-auto w-5/6'>
 						<Col>
-							<img className='p-1' alt='' src='image1.jpg' />
-							<img className='p-1' alt='' src='image3.jpg' />
+							<img
+								className='p-1'
+								alt='students in a classroom listening to a presentation'
+								src='image1.jpg'
+							/>
+							<img
+								className='p-1'
+								alt='students in a classroom listening to a presentation'
+								src='image3.jpg'
+							/>
 						</Col>
 						<Col>
-							<img className='p-1' alt='' src='image16.png' />
-							<img className='p-1' alt='' src='image5.jpg' />
+							<img
+								className='p-1'
+								alt='students in a classroom listening to a panel'
+								src='image16.png'
+							/>
+							<img
+								className='p-1'
+								alt='groups of students in discussion sitting around tables'
+								src='image5.jpg'
+							/>
 						</Col>
 					</Col>
 				</Row>
 				<Row>
 					<Col className='flex justify-center m-auto w-5/6'>
 						<Col>
-							<img className='p-1' alt='' src='image15.png' />
-							<img className='p-1' alt='' src='image14.png' />
+							<img
+								className='p-1'
+								alt='students sitting at tables listening to a presentation'
+								src='image15.png'
+							/>
+							<img className='p-1' alt='students in a large classroom watching a presentation' src='image14.png' />
 						</Col>
 						<Col>
-							<img className='p-1' alt='' src='image17.png' />
-							<img className='p-1' alt='' src='image9.jpg' />
+							<img
+								className='p-1'
+								alt='students watching a presentation on development workflow'
+								src='image17.png'
+							/>
+							<img className='p-1' alt='students following along on their laptops in an acm workshop' src='image9.jpg' />
 						</Col>
 					</Col>
 					<Col className='py-2'>
-						<img alt='' src='image10.jpg' />
+						<img alt='students sitting at tables discussing their ideas with professors' src='image10.jpg' />
 					</Col>
 				</Row>
 				<Row>
-					<img className='pt-2 pb-16' alt='' src='image12.png' />
+					<img className='pt-2 pb-16' alt='students paying attention during an acm meeting' src='image12.png' />
 				</Row>
 			</Row>
 		</Row>
