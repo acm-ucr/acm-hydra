ced9e9ba5b972a33bfb613b52275c9445c08a0cb
Raidah Fairooz <rfair008@ucr.edu>
2022-09-03 10:27:06 -0700

    changed the planding picture name and made changes in the code accordingly

--- components/Landing.js
+++ components/Landing.js
@@ -3,11 +3,7 @@ import React from "react";
 const Landing = () => {
 	return (
 		<div className='flex justify-center items-center bg-acm-white w-full mx-auto'>
-			<img
-				className='w-11/12'
-				src='welcomeToACM@UCR.png'
-				alt='placeholder'
-			></img>
+			<img className='w-11/12' src='keyboard.png' alt='placeholder'></img>
 		</div>
 	);
 };
