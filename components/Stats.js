4c662c78485e50f64cf3bfb95d014e3c8bf4955a
Val <vwong031@ucr.edu>
2022-09-02 17:25:25 -0700

    remove stats width. moving changes to different branch. ran prettier

--- components/Stats.js
+++ components/Stats.js
@@ -3,7 +3,7 @@ import { Row, Col } from "react-bootstrap";
 
 const Stats = () => {
 	return (
-		<Row className='font-lexend text-5xl decoration-black font-semibold flex justify-between items-center whitespace-nowrap mx-auto w-11/12'>
+		<Row className='font-lexend text-5xl decoration-black font-semibold flex justify-between items-center whitespace-nowrap'>
 			<Col
 				sm={12}
 				md={6}
