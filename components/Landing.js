1ea1caea1df2c356cc30c15e4395ba66b861f336
Raidah Fairooz <rfair008@ucr.edu>
2022-09-03 01:05:13 -0700

    added the lwlanding welcome pic, white background for right now

--- components/Landing.js
+++ components/Landing.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Landing = () => {
-	return <div>Landing</div>;
+	return (
+		<div className='flex justify-center items-center bg-acm-white w-full mx-auto'>
+			<img
+				className='w-11/12'
+				src='welcomeToACM@UCR.png'
+				alt='placeholder'
+			></img>
+		</div>
+	);
 };
 
 export default Landing;
