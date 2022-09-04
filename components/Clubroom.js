8ef23b895e2762499f714817e5b6c5fc1bdd8c87
Val <vwong031@ucr.edu>
2022-09-03 21:49:30 -0700

    added filler text & image. resized width to be same as faq. ran prettier

--- components/Clubroom.js
+++ components/Clubroom.js
@@ -1,10 +1,22 @@
 import React from "react";
-import { Row } from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 
 const Clubroom = () => {
 	return (
-		<Row className='flex justify-center bg-acm-blue mx-auto w-11/12'>
-			test
+		<Row className='flex justify-center bg-acm-blue rounded-2xl mx-auto w-2/3 px-4 py-5 mt-4'>
+			<Col className='font-lexend text-acm-black font-medium'>
+				<p className='text-3xl'>ACM Clubroom</p>
+				<p className='text-xl'>blah</p>
+				<p className='text-xl'>
+					here are the perks:
+					<>
+						<li className='pl-5'></li>
+					</>
+				</p>
+			</Col>
+			<Col>
+				<img alt='' src='image3.jpg' />
+			</Col>
 		</Row>
 	);
 };
