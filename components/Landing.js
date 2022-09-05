555fbdd9c0f7304b1ec1f217188c70f5a96cb795
Raidah Fairooz <rfair008@ucr.edu>
2022-09-05 04:48:26 -0700

    changed background to blue, added top padding

--- components/Landing.js
+++ components/Landing.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Landing = () => {
 	return (
-		<div className='flex justify-center items-center bg-acm-white w-full mx-auto'>
+		<div className='flex justify-center items-center bg-acm-blue w-full mx-auto py-12'>
 			<img className='w-11/12' src='keyboard.png' alt='placeholder'></img>
 		</div>
 	);
