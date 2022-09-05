a4d8c2104c079c380be368cae39a0ae23e38f0b0
Val <vwong031@ucr.edu>
2022-09-04 21:55:24 -0700

    removed couch perk & changed description

--- components/Clubroom.js
+++ components/Clubroom.js
@@ -7,23 +7,19 @@ const Clubroom = () => {
 			<Col sm={6} className='font-lexend text-acm-black font-medium'>
 				<p className='text-3xl'>ACM Clubroom</p>
 				<p className='text-xl'>
-					{
-						"The club room is the premier spot for members to work, socialize, or do a bit of both! We'll often hold workshops and socials in the same room, so it's a good place to know about. You'll also find members of other clubs hanging out here, so it's a great spot to expand your network. It's open almost 24/7, with times varying every quarter."
-					}
+					We will often hold academic, professional, and technical
+					workshops and events in this room! Located in Wingston Chung
+					Hall 127, behind Bytes, you can expect a lively environment
+					to study, socialize, and learn new things!
 				</p>
 				<p className='text-xl'>
 					Here are the perks:
 					<>
-						<li className='pl-5'>
-							Full whiteboard walls and tables
-						</li>
+						<li className='pl-5'>Whiteboard walls</li>
 						<li className='pl-5'>Programming books</li>
 						<li className='pl-5'>Lockers</li>
 						<li className='pl-5'>Snacks</li>
 						<li className='pl-5'>Drinks</li>
-						<li className='pl-5'>
-							A couch to nap on between classes
-						</li>
 					</>
 				</p>
 			</Col>
