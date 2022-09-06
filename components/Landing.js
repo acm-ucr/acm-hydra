7b89d9d28187babb632768b324d49f22444f6d1f
Raidah Fairooz <rfair008@ucr.edu>
2022-09-06 01:53:38 -0700

    landing pic takes up the entire screen size

--- components/Landing.js
+++ components/Landing.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Landing = () => {
 	return (
-		<div className='flex justify-center items-center bg-acm-blue w-full mx-auto py-12'>
+		<div className='flex justify-center items-center bg-acm-blue w-full mx-auto py-12 h-[100vh]'>
 			<img className='w-11/12' src='keyboard.png' alt='placeholder'></img>
 		</div>
 	);
