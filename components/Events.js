1756a0e8e0f57b6992d58f73af655a6fa9b72ab3
Raidah Fairooz <rfair008@ucr.edu>
2022-09-06 20:13:49 -0700

    shorter titles and descriptions

--- components/Events.js
+++ components/Events.js
@@ -7,13 +7,13 @@ const filters = {
 
 export const Events = [
 	{
-		title: "Intern/UG Research Panel",
+		title: "Intern/Research Panel",
 		location: "WCH 205/206",
 		color: filters["Meetings"],
 		start: new Date(2022, 8, 27, 16, 0, 0),
 		end: new Date(2022, 8, 27, 18, 0, 0),
 		description:
-			"Are you someone searching for internships or research opportunities? For our first GM, we’ll be holding our annual intern & research panel. This is a great opportunity to ask questions and hear advice from students that have landed internship and undergraduate research positions.",
+			"Learn about the tips and tricks undergraduates use to secure internships and research opportunities.",
 	},
 	{
 		title: "CRS: Resume + LaTex",
@@ -24,13 +24,13 @@ export const Events = [
 		description: "REALLY COOL DESCRIPTION THAT WE NEED TO FIGURE OUT",
 	},
 	{
-		title: "Metaverse With Meta: Meta Panel",
+		title: "Metaverse",
 		location: "WCH 205/206",
 		color: filters["Meetings"],
 		start: new Date(2022, 9, 11, 16, 0, 0),
 		end: new Date(2022, 9, 11, 18, 0, 0),
 		description:
-			"The metaverse is a concept about which we know extremely little. Is it the future of the internet? Or a video game? Or a deeply uncomfortable version of Zoom? Join us for our second GM, as we’ll be holding a panel with Meta software engineers, discussing what is the metaverse, how social interaction will change, and their experience working under a MANGA company.",
+			"Explore the future of the metaverse with Meta software engineers and learn about its impacts in social interaction.",
 	},
 	{
 		title: "CRS: Internships",
@@ -71,7 +71,7 @@ export const Events = [
 		start: new Date(2022, 10, 8, 16, 0, 0),
 		end: new Date(2022, 10, 8, 18, 0, 0),
 		description:
-			"When getting in computer science, everyone learns a new programming language or how to develop their first web app. But despite all the leetcoding and CS finals, no one wonders how to THINK like a programmer. If you’re interested in learning how to think like one, come to our third GM where we’ll discuss with a professor(TBA) on tips and tricks for thinking like a programmer.",
+			"Discover the tips and tricks to THINK like a programmer and help you excel in your classes, projects, and various opportunities.",
 	},
 	{
 		title: "CRS: Mock Interviews",
@@ -90,13 +90,13 @@ export const Events = [
 		description: "REALLY COOL DESCRIPTION THAT WE NEED TO FIGURE OUT",
 	},
 	{
-		title: "Running a Coding YouTube Channel: Interview with Namanh Kapur",
+		title: "Interview with Namanh Kapur",
 		location: "WCH 205/206",
 		color: filters["Meetings"],
 		start: new Date(2022, 10, 22, 16, 0, 0),
 		end: new Date(2022, 10, 22, 18, 0, 0),
 		description:
-			"Youtube has over a million coding videos and counting, with thousands of CS professionals posting content about learning how to code or how to surpass and interview for a MANGA company. Join us for our final virtual GM as we hold an interview with Youtube content creator and Ex-Microsoft engineer Namanh Kapur, discussing his journey towards making videos on Youtube, his advice towards future software engineers entering the industry, and his cool tips on starting a Youtube channel and making content.",
+			"Join Namanh Kapur, a content creator, discuss his journey and various challenges he faced along the way.",
 	},
 	{
 		title: "CRS: Research",
