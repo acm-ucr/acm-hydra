de91e36cf173c93628104499f491e2d7be11b759
Divyank Shah <divyank.shah.2016@gmail.com>
2022-09-26 17:45:43 -0700

    metadata

--- pages/index.js
+++ pages/index.js
@@ -5,17 +5,21 @@ import Stats from "../components/Stats";
 import Upcoming from "../components/Upcoming";
 import Learn from "../components/Learn";
 import Inspire from "../components/Inspire";
+import Head from "next/head";
 
 const IndexPage = () => {
 	return (
-		<div>
+		<>
+			<Head>
+				<title>ACM at UCR</title>
+			</Head>
 			<Landing />
 			<Introduction />
 			<Stats />
 			<Upcoming />
 			<Learn />
 			<Inspire />
-		</div>
+		</>
 	);
 };
 
