0a0a3cf46aaf469f7a76feb4dd6d272cab980459
jvarCS <vargassegoviano@gmail.com>
2023-02-07 23:20:01 -0800

    next/font implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,23 @@
+/* eslint-disable new-cap */
 import "../styles/globals.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "../components/Layout";
+import { Lexend } from "@next/font/google";
+
+const lexend = Lexend({
+	subsets: ["latin"],
+	variable: "--font-lexend",
+	weight: "500",
+});
 
 function MyApp({ Component, pageProps }) {
 	return (
-		<Layout>
-			<Component {...pageProps} />
-		</Layout>
+		<main className={`${lexend.variable}`}>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</main>
 	);
 }
 
