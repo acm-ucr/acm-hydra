7dfcea74fc7266c2c2b4b69307500423198c02df
jvarCS <vargassegoviano@gmail.com>
2023-03-06 17:08:24 -0800

    Added title check

--- cypress/e2e/project_presence.cy.js
+++ cypress/e2e/project_presence.cy.js
@@ -2,6 +2,7 @@ describe("Project Presence Check", () => {
 	it("passes", () => {
 		cy.visit("http://localhost:3000");
 		cy.scrollTo("bottom");
+		cy.get(":nth-child(5) > .justify-center");
 		cy.get(".shadow-acm-green");
 		cy.get(".shadow-acm-lightblue");
 		cy.get(".shadow-acm-purple");
