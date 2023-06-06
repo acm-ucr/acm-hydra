0947f6fa48cab06e6fba0503b7add9cf82277271
jvarCS <vargassegoviano@gmail.com>
2023-06-06 00:45:54 -0700

    fix

--- cypress/e2e/board_pCheck.cy.js
+++ cypress/e2e/board_pCheck.cy.js
@@ -20,8 +20,6 @@ describe("board presence check", () => {
 		cy.get(".bg-acm-marine").contains("Membership");
 		cy.get(":nth-child(8) > .bg-acm-blue").contains("Joshua Hsu");
 		cy.get(":nth-child(8) > .bg-acm-blue").contains("Industry");
-		cy.get(":nth-child(9) > .bg-acm-lightpurple").contains("Justin Yee");
-		cy.get(":nth-child(9) > .bg-acm-lightpurple").contains("Alumni");
 		cy.get(":nth-child(10) > .bg-acm-yellow").contains("Noah Snodgrass");
 		cy.get(":nth-child(10) > .bg-acm-yellow").contains("Treasurer");
 		cy.get(":nth-child(11) > .bg-acm-green").contains("Kapilan Ramasamy");
