2dd7aa557064e567448d7e6ce7aac7f822587d32
jvarCS <vargassegoviano@gmail.com>
2023-06-14 23:02:02 -0700

    added padding and increased blue bg coverage

--- components/Navigation.js
+++ components/Navigation.js
@@ -26,42 +26,42 @@ const Navigation = () => {
 					<Nav.Link
 						eventkey='6'
 						href='/'
-						className='mb-0 py-1 px-2 no-underline ml-auto text-2xl whitespace-nowrap bg-acm-white w-full text-center !text-acm-black !font-medium hover:!text-acm-blue hover:cursor-pointer'
+						className='mb-0 py-1 px-3 no-underline ml-auto text-2xl whitespace-nowrap bg-acm-white w-full text-center !text-acm-black !font-medium hover:!text-acm-blue hover:cursor-pointer'
 					>
 						home
 					</Nav.Link>
 					<Nav.Link
 						eventkey='2'
 						href='/about'
-						className='mb-0 py-1 px-2 no-underline ml-auto text-2xl whitespace-nowrap bg-acm-white w-full text-center !text-acm-black !font-medium hover:!text-acm-blue hover:cursor-pointer'
+						className='mb-0 py-1 px-3 no-underline ml-auto text-2xl whitespace-nowrap bg-acm-white w-full text-center !text-acm-black !font-medium hover:!text-acm-blue hover:cursor-pointer'
 					>
 						about
 					</Nav.Link>
 					<Nav.Link
 						eventkey='3'
 						href='/board'
-						className='mb-0 py-1 px-2 no-underline ml-auto text-2xl whitespace-nowrap bg-acm-white w-full text-center !text-acm-black !font-medium hover:!text-acm-blue hover:cursor-pointer'
+						className='mb-0 py-1 px-3 no-underline ml-auto text-2xl whitespace-nowrap bg-acm-white w-full text-center !text-acm-black !font-medium hover:!text-acm-blue hover:cursor-pointer'
 					>
 						board
 					</Nav.Link>
 					<Nav.Link
 						eventkey='4'
 						href='/events'
-						className='mb-0 py-1 px-2 no-underline ml-auto text-2xl whitespace-nowrap bg-acm-white w-full text-center !text-acm-black !font-medium hover:!text-acm-blue hover:cursor-pointer'
+						className='mb-0 py-1 px-3 no-underline ml-auto text-2xl whitespace-nowrap bg-acm-white w-full text-center !text-acm-black !font-medium hover:!text-acm-blue hover:cursor-pointer'
 					>
 						events
 					</Nav.Link>
 					<Nav.Link
 						eventkey='5'
 						href='/join'
-						className='mb-0 py-1 px-2 no-underline ml-auto text-2xl whitespace-nowrap bg-acm-white w-full text-center !text-acm-black !font-medium hover:!text-acm-blue hover:cursor-pointer'
+						className='mb-0 py-1 px-3 no-underline ml-auto text-2xl whitespace-nowrap bg-acm-white w-full text-center !text-acm-black !font-medium hover:!text-acm-blue hover:cursor-pointer'
 					>
 						join us
 					</Nav.Link>
 					<Nav.Link
 						eventkey='7'
 						href='https://membership-portal-prod-wyhkctdt6a-uw.a.run.app'
-						className='mb-0 py-1 px-2 no-underline ml-2 text-2xl whitespace-nowrap bg-acm-blue w-full text-center !text-acm-black !font-medium hover:!text-acm-white hover:cursor-pointer rounded-xl'
+						className='mb-0 py-1 px-3 no-underline ml-2 text-2xl whitespace-nowrap bg-acm-blue w-full text-center !text-acm-black !font-medium hover:!text-acm-white hover:cursor-pointer rounded-xl'
 					>
 						login
 					</Nav.Link>
