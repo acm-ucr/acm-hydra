942d4c0cefa01148cb5215313bba4d5c0bdf7a71
Divyank Shah <divyank.shah.2016@gmail.com>
2023-10-02 22:36:28 -0700

    fix api call

--- components/Upcoming.js
+++ components/Upcoming.js
@@ -1,12 +1,102 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Event from "./Event";
-import EventsContext from "./EventContext";
-import { useContext } from "react";
+import axios from "axios";
+
+const colorMappings = {
+	social: "!bg-acm-red",
+	career: "!bg-acm-lightpurple",
+	general: "!bg-acm-blue",
+	technical: "!bg-acm-orange",
+	academic: "!bg-acm-marine",
+};
+
+const colorMappingsText = {
+	social: "text-acm-red",
+	career: "text-acm-lightpurple",
+	general: "text-acm-blue",
+	technical: "text-acm-orange",
+	academic: "text-acm-marine",
+};
+
+const colorMappingsBorder = {
+	social: "border-acm-red",
+	career: "border-acm-lightpurple",
+	general: "border-acm-blue",
+	technical: "border-acm-orange",
+	academic: "border-acm-marine",
+};
+
+const colorMappingsShadow = {
+	social: "shadow-acm-red",
+	career: "shadow-acm-lightpurple",
+	general: "shadow-acm-blue",
+	technical: "shadow-acm-orange",
+	academic: "shadow-acm-marine",
+};
 
 const Upcoming = ({ size }) => {
-	const { events } = useContext(EventsContext);
+	const [events, setEvents] = useState([]);
+
+	useEffect(() => {
+		axios
+			.get(
+				`https://www.googleapis.com/calendar/v3/calendars/${
+					process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_EMAIL
+				}/events?key=${
+					process.env.NEXT_PUBLIC_GOOGLE_CALENDAR_API_KEY
+				}&singleEvents=true&orderBy=starttime&timeMin=${new Date().toISOString()}&maxResults=${size}`
+			)
+			.then(response => {
+				const calendarEvents = response.data.items.filter(a => {
+					a.start = new Date(a.start.dateTime);
+					a.end = new Date(a.end.dateTime);
+
+					a.color =
+						colorMappings[
+							`${a.description
+								.split(" ")[0]
+								.toLowerCase()
+								.replace(":", "")}`
+						];
+
+					a.textColor =
+						colorMappingsText[
+							`${a.description
+								.split(" ")[0]
+								.toLowerCase()
+								.replace(":", "")}`
+						];
+
+					a.border =
+						colorMappingsBorder[
+							`${a.description
+								.split(" ")[0]
+								.toLowerCase()
+								.replace(":", "")}`
+						];
+
+					a.shadow =
+						colorMappingsShadow[
+							`${a.description
+								.split(" ")[0]
+								.toLowerCase()
+								.replace(":", "")}`
+						];
+
+					return (
+						a.description.startsWith("General:") ||
+						a.description.startsWith("Technical:") ||
+						a.description.startsWith("Social:") ||
+						a.description.startsWith("Career:") ||
+						a.description.startsWith("Academic:")
+					);
+				});
+
+				setEvents(calendarEvents);
+			});
+	}, [size]);
 
 	return (
 		<div className='w-full mb-20 flex justify-content items-center flex-col'>
