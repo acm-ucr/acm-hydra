a67e4282258ee73ee0287931ab35037520d424c8
Stanley Lew <stanley43595@gmail.com>
2024-04-09 23:23:49 -0700

    updated board page

--- data/Colors.js
+++ data/Colors.js
@@ -1,11 +1,11 @@
 const Colors = [
-	"bg-acm-blue",
-	"bg-acm-lightpurple",
-	"bg-acm-yellow",
+	"bg-acm-marine",
 	"bg-acm-green",
+	"bg-acm-lightpurple",
+	"bg-acm-blue",
 	"bg-acm-pink",
 	"bg-acm-orange",
-	"bg-acm-marine",
+	"bg-acm-yellow",
 ];
 
 export default Colors;
