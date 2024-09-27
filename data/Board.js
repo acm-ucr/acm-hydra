f529de90034b1b00f91f2201a2e5dd44a9fbb98a
Stanley Lew <stanley43595@gmail.com>
2024-09-27 14:20:06 -0700

    update sept 27

--- data/Board.js
+++ data/Board.js
@@ -80,6 +80,14 @@ const Board = [
 		linkedin: "https://www.linkedin.com/in/suryateja-duvvuri-22b377162/",
 		github: "https://github.com/SuryatejaDuvvuri",
 	},
+	{
+		name: "Taaha Sayed",
+		position: "Computing Director",
+		image: "taaha2024.webp",
+		email: "msaye007@ucr.edu",
+		linkedin: "https://linkedin.com/in/taaha-sayed",
+		github: "https://github.com/Taahh",
+	},
 ];
 
 export default Board;
