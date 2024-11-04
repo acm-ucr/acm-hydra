fed74a02cfb308f693e81975ceae0a154f78cab2
Stanley Lew <stanley43595@gmail.com>
2024-11-04 15:03:56 -0800

    add create link

--- components/Opportunities.jsx
+++ components/Opportunities.jsx
@@ -5,15 +5,26 @@ const Opportunities = () => {
 			<p className=' text-center text-acm-black font-lexend text-[14vw] md:text-[3.5vw] font-semibold py-[5vh]'>
 				Programs
 			</p>
-			<Link
-				href='https://tinyurl.com/acmignitewinter25'
-				className='no-underline'
-				target='_blank'
-			>
-				<div className='bg-acm-lightblue hover:opacity-85 text-acm-white text-[4vw] md:text-[2vw] px-8 py-4 rounded-xl shadow-xl text-center font-bold'>
-					ACM Ignite Winter&apos;25 Application
-				</div>
-			</Link>
+			<div className='grid grid-cols-1 md:grid-cols-2 gap-4'>
+				<Link
+					href='https://tinyurl.com/acmignitewinter25'
+					className='no-underline'
+					target='_blank'
+				>
+					<div className='bg-gradient-to-br from-acm-lightblue to-acm-lightpurple hover:opacity-85 text-acm-white text-[4vw] md:text-[2vw] px-8 py-4 rounded-xl shadow-xl text-center font-bold'>
+						ACM Ignite Winter&apos;25 Application
+					</div>
+				</Link>
+				<Link
+					href='https://tinyurl.com/acmcreatewinter25'
+					className='no-underline'
+					target='_blank'
+				>
+					<div className='bg-gradient-to-br from-acm-lightpurple to-acm-pink hover:opacity-85 text-acm-white text-[4vw] md:text-[2vw] px-8 py-4 rounded-xl shadow-xl text-center font-bold'>
+						ACM Create Winter&apos;25 Application
+					</div>
+				</Link>
+			</div>
 		</div>
 	);
 };
